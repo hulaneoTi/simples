0d921ab21e606abdfcec18af50b9bb66a452c133
hulaneoTi <hulaneo@gmail.com>
2022-05-13 14:39:04 -0300

    Update bloqueiasite.bat

--- bloqueiasite.bat
+++ bloqueiasite.bat
@@ -3,7 +3,7 @@ if not "%1"=="am_admin" (
     powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
     exit /b
 )
-echo "digite REMOVER para remover o bloqueio"
+echo digite REMOVER para remover o bloqueio
 set /p site="Qual site deseja bloquear: "
 if "%site%"=="remover" (goto remove)
 :add
